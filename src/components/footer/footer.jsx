7d10002951cbef08368e628eeb25b3d42632fcd2
Billy McCrackin <44419353+bmc1995@users.noreply.github.com>
2022-11-06 20:03:26 -0700

    feat(create-components): add home, header, footer

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <footer>
+            <h6 style={{'textAlign': 'center'}}>With {'<3'} by Billy</h6>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ src/pages/home/home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const Home = () => {
+    return (
+        <div style={{'textAlign': 'center'}}>
+            <h1>Hi, I'm Billy.</h1>
+            <h3>I like to create software.</h3>
+            <h5>This is my portfolio site.</h5>
+        </div>
+    )
+}
+
+export default Home
