2a7b41e4d941ff14abe898010aa1e06a40fd54c1
Billy McCrackin <44419353+bmc1995@users.noreply.github.com>
2022-11-22 21:39:23 -0700

    test(App): Update test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react';
+import Home from './pages/home/home';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders portfolio image', () => {
+  render(<Home />);
+  const portfolioImg = screen.getByAltText(/Billy/)
+  expect(portfolioImg).toBeInTheDocument();
 });
